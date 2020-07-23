1c62e5b57c7e2bf34fc502d5c4d690c6eb884365
sanaeltahhan <sana.eltahhan@hotmail.fr>
2020-07-23 12:02:26 +0200

    TP 01 fonctions - Variable de type fonction

--- 01.function.js
+++ 01.function.js
@@ -1,6 +1,7 @@
 // test fonctionnement console
 //console.log("01 - Fonctions");
 
+// ################################### Définition d’une fonction ##############################
 // Initialisation variables
 var nombre1 = 10;
 var nombre2 = 20;
@@ -10,7 +11,33 @@ function additionner(nb1, nb2) {
     return nb1+nb2;
 }
 
-// resultat additionner avec nombre1 et nombre2
-resulta1 = additionner(nombre1,nombre2);
+// Créer une variable resultat1 dont le résultat est l’invocation de la fonction additionner avec les paramètres nombre1 et nombre2.
+resultat1 = additionner(nombre1,nombre2);
 
-console.log("resulta1 =", resulta1);
+// Affichage
+console.log("resultat1 =", resultat1);
+
+// ################################## Variable de type fonction ####################################
+
+// Créer une variable somme dont la valeur est une référence à la fonction additionner.
+var somme = additionner;
+
+//Créer une variable resultat2 dont le résultat est l’invocation de la fonction somme avec les paramètres nombre1 et nombre2.
+resultat2 = somme(nombre1,nombre2);
+
+// Affichage
+console.log("resultat2 =", resultat2);
+
+// Methode multiplication
+function multiplication(nb1, nb2) {
+    return nb1 * nb2;
+}
+
+// Créer une variable multiplication dont la valeur est une fonction qui effectue la multiplication entre 2 nombres.
+var multiplication = multiplication;
+
+// Créer une variable resultat3 dont le résultat est l’invocation de la fonction multiplication avec les paramètres nombre1 et nombre2.
+resultat3 = multiplication(nombre1,nombre2);
+
+// Affichage 
+console.log("resultat3 = " , resultat3);
