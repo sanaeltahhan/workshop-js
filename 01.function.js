431c9350ae5e70b58e947cdfb97f829bdb84bd87
sanaeltahhan <sana.eltahhan@hotmail.fr>
2020-07-23 11:42:59 +0200

    TP 01 fonctions - definition d'une fonction

---
+++ 01.function.js
@@ -0,0 +1,16 @@
+// test fonctionnement console
+//console.log("01 - Fonctions");
+
+// Initialisation variables
+var nombre1 = 10;
+var nombre2 = 20;
+
+// Methode additionner 
+function additionner(nb1, nb2) {
+    return nb1+nb2;
+}
+
+// resultat additionner avec nombre1 et nombre2
+resulta1 = additionner(nombre1,nombre2);
+
+console.log("resulta1 =", resulta1);
