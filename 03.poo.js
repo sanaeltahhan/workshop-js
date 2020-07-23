47108d280ab76f01cfc745fb87e51ec321935e49
sanaeltahhan <sana.eltahhan@hotmail.fr>
2020-07-23 16:06:44 +0200

    TP 03 - POO - Ajouter une prop à personne

--- 03.poo.js
+++ 03.poo.js
@@ -1,15 +1,20 @@
 // Fonction constructeur
-function Personne(nom, prenom, pseudo) {
+function Personne(nom, prenom, pseudo, age) {
     this.nom = nom; 
     this.prenom = prenom;
     this.pseudo = pseudo; 
+    this.age = age;
 
     this.getNomComplet = function() {
          return this.nom +" "+ this.prenom +" "+ this.pseudo;
     }
 }
 
-var personne1 = new Personne ("Jules", "LEMAIRE", "jules44");
+// ajout prototype aga
+
+Personne.prototype.age = "NON RENSEIGNE";
+
+var personne1 = new Personne ("Jules", "LEMAIRE", "jules44", 30);
 
 var personne2 = new Personne ("Paul", "LEMAIRE", "paul44");
 
@@ -27,3 +32,5 @@ afficherPersonne(personne1);
 
 console.log("Personne2 = ") ;
  afficherPersonne(personne2);
+
+ console.log(personne1.age);
